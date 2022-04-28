1fd6305cfae3286bad9e49c734ff7650a98cef90
Sibin Thomas <54390036+SibinThomasQuad@users.noreply.github.com>
2022-04-28 17:03:24 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <title>How to Upload Image using ckeditor in PHP</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <script src="https://cdn.ckeditor.com/4.6.2/standard-all/ckeditor.js"></script>
+ </head>
+ <body>
+  <div class="container">
+   <br />
+   <h3 align="center">How to Upload Image using ckeditor in PHP</h3>
+   <br />
+   <div class="panel panel-default">
+    <div class="panel-heading">
+     <h3 class="panel-title">How to Upload Image using ckeditor in PHP</h3>
+    </div>
+    <div class="panel-body">
+     <textarea name="content" id="content" class="form-control ckeditor"></textarea>
+    </div>
+   </div>
+  </div>
+  <button onclick='data();'>SAVE</button>
+ </body>
+</html>
+
+<script>
+ CKEDITOR.replace( 'content', {
+  height: 300,
+  filebrowserUploadUrl: "upload.php"
+ });
+</script>
+<script>
+    function data()
+    {
+    alert(CKEDITOR.instances.content.getData());
+    }
+</script>
